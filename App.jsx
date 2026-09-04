756fa64544cf68512cf4cda887c17658c0b4f19f
ashishkumar68466-glitch <ashishkumar68466@gmail.com>
2026-09-05 00:39:25 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/Navbar.jsx'
+import Navbar from "./Navbar.jsx";
 import Hero from './components/Hero.jsx'
 import Stats from './components/Stats.jsx'
 import Services from './components/Services.jsx'
